0df6dcc470b6217e0753ff73174d406cc63cc936
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2025-09-16 19:33:28 +0300

    Update of the state is asynchronous

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,40 +1,41 @@
 import { useState } from 'react'
 
-const Button = ({onClick, text}) => (
-  <button onClick={onClick}>
-    {text}
-    </button>
-)
-const Display = ({counter}) => <div>{counter}</div>
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('Rendering with counter value', counter)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  
-  const increaseTo0ne = () => {
-    console.log('inceasing, value before,', counter)
-    setCounter(counter + 1)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    console.log('left before', left)
+    const updateLeft = left + 1
+    setLeft(updateLeft)
+    console.log('left after', updateLeft)
+    setTotal(left + right)
   }
 
-  const decreaseTo0ne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    console.log('right before', right)
+    const updateRight = right + 1
+    setRight(updateRight)
+    console.log('right after', updateRight)
+    setTotal(left + right)
   }
 
-  const reseToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-  return (
+  return(
     <div>
-    <Display counter={counter}/>
-    <Button onClick={increaseTo0ne} text='plus'/>
-    <Button onClick={decreaseTo0ne} text='minus'/>
-    <Button onClick={reseToZero} text='zero'/>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
+
 }
 
 export default App
