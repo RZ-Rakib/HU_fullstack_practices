8d28bd2efb01b72196b702afc6319dadc69517e6
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2025-10-19 16:11:50 +0300

    updated code

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -13,18 +13,29 @@ const App = () => {
       .then(allObjects => {
         setNotes(allObjects)
       })
+      .catch(error => {
+        alert(`Something went wrong to the server. failed fetching data${error}`)
+        console.log("error ==> ", error);
+      })
   }
   useEffect(hook, [])
 
   const noteToShow = showAll
     ? notes
-    : notes.filter(n => n.important === true)
+    : notes.filter(note => note.important === true)
 
   const addNote = (event) => {
     event.preventDefault()
+    const normalize = newNote.trim().toLowerCase()
+
+    if (notes.some(note => note.content.toLowerCase() === normalize)) {
+      alert(`${newNote} is already exist in the server.`)
+      return
+    }
     const newObject = {
       content: newNote,
       important: Math.random() < 0.5,
+      vote: 0
     }
     noteService
       .create(newObject)
@@ -32,24 +43,42 @@ const App = () => {
         setNotes(notes.concat(createdObject))
         setNewNote('')
       })
-
+      .catch(error => {
+        alert(`failed to add new note`)
+        console.log("error ==> ", error);
+      })
   }
 
   const handleNewNote = (event) => {
     setNewNote(event.target.value)
-    console.log("event.target.value ==> ", event.target.value);
   }
 
   const toggleImportance = (id) => {
-    const note = notes.find(n => n.id === id)
+    const note = notes.find(note => note.id === id)
     const updatedNote = { ...note, important: !note.important }
 
     noteService
       .update(id, updatedNote)
       .then(updatedObject => {
-        setNotes(notes.map(n => n.id === id ? updatedObject : n))
+        setNotes(notes.map(note => note.id === id ? updatedObject : note))
       })
+      .catch(error => {
+        alert(`Error happend to update ${updatedNote}. ${error}`)
+        setNotes(notes.filter(note => note.id !== id))
+      })
+  }
+  const handleVotes = (id) => {
+    const voteNote = notes.find(note => note.id === id)
+    const updatedVoteNote = { ...voteNote, vote: (voteNote.vote ?? 0) + 1 }
 
+    noteService
+      .update(id, updatedVoteNote)
+      .then(updatedNote => {
+        setNotes(notes.map(note => note.id === id ? updatedNote : note))
+      })
+      .catch(error => {
+        console.log("error ==> ", error)
+      })
   }
 
   return (
@@ -60,7 +89,7 @@ const App = () => {
       </button>
       <ul>
         {noteToShow.map(n =>
-          <Note key={n.id} note={n} toggleImportance={() => toggleImportance(n.id)} />)}
+          <Note key={n.id} note={n} toggleImportance={() => toggleImportance(n.id)} handleVotes={() => handleVotes(n.id)} />)}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNewNote} />
--- part2/src/component/Note.jsx
+++ part2/src/component/Note.jsx
@@ -1,8 +1,10 @@
 /**
  * @prop {object} note - this object contains content and important
- * @prop {function} toggleImportance - an event handler function to toggle important
+ * @prop {function} toggleImportance - an event handler function 
+ * to toggle important
+ * @prop {function} handleVotes - an function to handle vote by increasing 1 vote after each sucessfull vote pressing by the button
  */
-const Note = ({ note, toggleImportance }) => {
+const Note = ({ note, toggleImportance, handleVotes }) => {
   const label =
     note.important === true
       ? 'make not important'
@@ -11,8 +13,10 @@ const Note = ({ note, toggleImportance }) => {
   return (
     <div>
       <li>
-        {note.content}-
+        {note.content}{' '}
         <button onClick={toggleImportance}>{label}</button>
+        {' '}{note.vote} Votes
+        <button onClick={handleVotes}>👍</button>
       </li>
     </div>
   )
