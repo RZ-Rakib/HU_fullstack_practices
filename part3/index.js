f597263f7e1e4930baaa0109061e884a9dcce60e
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2025-11-11 10:33:02 +0200

    changed morgan setup

--- part3/index.js
+++ part3/index.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const winston = require('./loggers/winston')
-const morganMiddleware = require('./loggers/morgan')
-const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
 
-app.use(morganMiddleware)
-
-app.use(cors({
-  origin: 'http://localhost:5173'
-}))
+app.use(morgan('combined'))
 
 // app.get('/', (req, res) => {
 //   res.send('Notes API is running successfully on Render!');
